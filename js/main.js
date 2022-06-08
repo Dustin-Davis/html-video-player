3989d439d851aa88646d9440448d5741d1d5ba4d
Dustin-Davis <drpookydavis@comcast.net>
2022-06-07 19:48:49 -0700

    toggled the playback state

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
+const playButton = document.getElementById('play');
 
 const videoWorks = !!document.createElement('video').canPlayType; // canPlayType is to detect support for a video format in a browser. !! = shorthand to make a boolean
-
 if (videoWorks) {
   video.controls = false;
   videoControls.classList.remove('hidden');
 }
+
+// togglePlay toggles the playback state of the video.
+// If the video playback is paused or ended, the video is played
+// otherwise, the video is paused
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+playButton.addEventListener('click', togglePlay);
